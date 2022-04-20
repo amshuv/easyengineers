ebbfacedf66fe2e76469b129f577d876e7758872
amshuv <104080046+amshuv@users.noreply.github.com>
2022-04-20 19:38:09 +0530

    Add files via upload

--- qa-app-limits.php
+++ qa-app-limits.php
@@ -12,4 +12,4 @@ if (defined('QA_DEBUG_PERFORMANCE') && QA_DEBUG_PERFORMANCE) {
 	trigger_error('Included file ' . basename(__FILE__) . ' is deprecated');
 }
 
-return (include QA_INCLUDE_DIR.'lang/qa-lang-emails.php');
+require_once QA_INCLUDE_DIR.'app/limits.php';
--- qa-class.smtp.php
+++ qa-class.smtp.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @deprecated This file is deprecated from Q2A 1.7; use the below file instead.
+ * @deprecated This file is deprecated; please use Q2A built-in functions for sending emails.
  */
 
 if (!defined('QA_VERSION')) {
@@ -12,4 +12,4 @@ if (defined('QA_DEBUG_PERFORMANCE') && QA_DEBUG_PERFORMANCE) {
 	trigger_error('Included file ' . basename(__FILE__) . ' is deprecated');
 }
 
-return (include QA_INCLUDE_DIR.'lang/qa-lang-emails.php');
+require_once 'vendor/PHPMailer/PHPMailerAutoload.php';
